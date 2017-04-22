6c7095f0b345b3f9de2da3eebd4a1b9cdcb549cf
suzziey108 <suzzie.yang@hotmail.com>
2017-04-22 20:31:23 +1200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,46 @@
+document.ready{
+function mySwitch(){
+        
+        alert("hello");
+    
+//   document.getElementById("passportselect").style.display="none";
+//            document.getElementById("onlineForm").style.display="none";
+//            document.getElementById("appForm").style.display="none";
+//            document.getElementById("upload").style.display="none";
+//          
+//        if(elmnt.className=="icon-online")
+//        {
+//            document.getElementById("onlineForm").style.display="block";
+//            if( document.getElementById("appType").className != "active")
+//                document.getElementById("appType").className += "active";
+//            
+//        }else if(elmnt.className=="icon-app"){
+//            
+//            document.getElementById("appForm").style.display="block";
+//            if( document.getElementById("appType").className != "active")
+//                document.getElementById("appType").className += "active";
+//            
+//            
+//        }else if(elmnt.id=="onlineNext"||elmnt.id=="appNext"){
+//    
+//            document.getElementById("upload").style.display="block";
+//            if(document.getElementById("photoProduct").className != "active")
+//                document.getElementById("photoProduct").className += "active";
+//        }else if(elmnt.id=="upNext"){
+//            document.getElementById("custDetail").style.display="block";
+//            if(document.getElementById("bUpload").className != "active")
+//                document.getElementById("bUpload").className += "active";
+//        }
+//               
+//       };
+    
+    }
+ 
+    
+}
+    
+    
+    
+    
+    
+
